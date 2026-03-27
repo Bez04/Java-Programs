6690f3a5a38ca2c3ef91299c274a3a798cd9872e
Bezalel Mathew <bezalelmathew659@gmail.com>
2026-03-27 20:54:10 +0530

    Add files via upload

---
+++ pkgOperation/Divide.java
@@ -0,0 +1,10 @@
+package pkgOperation;
+
+public class Divide implements Calculate{
+    public void cal(int x, int y)
+    {
+        System.out.println("Divide of Numbers :"+x+"/"+y+"="+(x/y));
+    
+} 
+    
+}
