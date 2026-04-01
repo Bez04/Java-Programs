671e57c7adc38df0d8b0fb24d405b7db6459831f
Bezalel Mathew <bezalelmathew659@gmail.com>
2026-04-01 22:10:36 +0530

    Add files via upload

---
+++ Myclient1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class Myclient1 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        DatagramSocket dSocket = new DatagramSocket();
+        InetAddress add = InetAddress.getByName("localhost");
+
+        String str = "* Message to Server from Client *";
+        byte[] bufBytes = str.getBytes();
+
+        DatagramPacket datagramPacket =
+            new DatagramPacket(bufBytes, bufBytes.length, add, 9000);
+
+        dSocket.send(datagramPacket);
+        dSocket.close();
+    }
+}
